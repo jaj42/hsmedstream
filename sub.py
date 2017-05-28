fa5a7f9b18135f11f5df1106fe9b0dab00a26469
jaj <jona@joachim.cc>
2017-05-28 22:02:08 +0200

    Rudimentary dump to csv

--- sub.py
+++ sub.py
@@ -1,7 +1,11 @@
+import csv
 import sys
-import zmq
-import msgpack
+import threading
 from time import sleep
+from datetime import datetime
+
+import msgpack
+import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
@@ -9,9 +13,24 @@ socket.setsockopt(zmq.SUBSCRIBE, b'')
 
 socket.connect ("tcp://127.0.0.1:5555")
 
+def go():
+    writer = None
+    firsttime = True
+    with open('ani.csv', 'w', newline='') as csvfile:
+        while True:
+            msg = socket.recv()
+            orig, msgpackdata = msg.split(b' ', 1)
+            unpacked = msgpack.unpackb(msgpackdata, encoding='utf-8')
+            unpacked.update({'datetime': str(datetime.now())})
+            if firsttime:
+                writer = csv.DictWriter(csvfile, fieldnames=list(unpacked.keys()))
+                writer.writeheader()
+                firsttime = False
+            writer.writerow(unpacked)
+            print(msgpackdata, unpacked)
+
+anithread = threading.Thread(target=go, daemon=True)
+anithread.start()
+
 while True:
-    msg = socket.recv()
-    orig, msgpackdata = msg.split(b' ', 1)
-    unpacked = msgpack.unpackb(msgpackdata)
-    print(msgpackdata, unpacked)
-    sleep(.1)
+    sleep(1)
