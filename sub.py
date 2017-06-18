5a73ab861759feb0ec1ec67afd6927f82921718c
jaj <jona@joachim.cc>
2017-06-18 19:18:24 +0200

    Fix interuptible infinite loop

--- sub.py
+++ sub.py
@@ -13,12 +13,19 @@ socket.setsockopt(zmq.SUBSCRIBE, b'')
 
 socket.connect ("tcp://127.0.0.1:5555")
 
+terminate = threading.Event()
+
 def go():
+    global terminate
     writer = None
     firsttime = True
     with open('ani.csv', 'w', newline='') as csvfile:
-        while True:
-            msg = socket.recv()
+        while not terminate.wait(timeout=.1):
+            try:
+                msg = socket.recv(flags=zmq.NOBLOCK)
+            except zmq.Again as e:
+                # No message received
+                continue
             orig, msgpackdata = msg.split(b' ', 1)
             unpacked = msgpack.unpackb(msgpackdata, encoding='utf-8')
             unpacked.update({'datetime': str(datetime.now())})
@@ -29,8 +36,13 @@ def go():
             writer.writerow(unpacked)
             print(msgpackdata, unpacked)
 
-anithread = threading.Thread(target=go, daemon=True)
+anithread = threading.Thread(target=go)
 anithread.start()
 
 while True:
-    sleep(1)
+    try:
+        sleep(1)
+    except KeyboardInterrupt:
+        terminate.set()
+        anithread.join()
+        break
