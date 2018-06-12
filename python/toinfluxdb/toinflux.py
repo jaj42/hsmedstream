b761086890e34ce3775ee0116f2493bf82173f45
jaj <jona@joachim.cc>
2018-06-12 23:46:03 +0200

    Handle numerics and waves concurrently

--- python/toinfluxdb/toinflux.py
+++ python/toinfluxdb/toinflux.py
@@ -6,21 +6,53 @@ import time
 from decimal import Decimal
 import pandas as pd
 
-host = '127.0.0.1'
-zmqport = 4202
+zmqhost = '127.0.0.1'
+numport = 4201
+wavport = 4202
+
+influxhost = '127.0.0.1'
 influxport = 8089
 
 #<measurement>[,<tag_key>=<tag_value>[,<tag_key>=<tag_value>]] <field_key>=<field_value>[,<field_key>=<field_value>] [<timestamp>]
 
-async def recvFromZmq(queue, host, port):
+def run():
+    loop = asyncio.get_event_loop()
+
+    numq = asyncio.Queue(loop=loop)
+    asyncio.ensure_future(recvFromZmq(loop, numq, zmqhost, numport))
+    asyncio.ensure_future(sendToInfluxdb(loop, numq, influxhost, influxport, numEncode))
+
+    wavq = asyncio.Queue(loop=loop)
+    asyncio.ensure_future(recvFromZmq(loop, wavq, zmqhost, wavport))
+    asyncio.ensure_future(sendToInfluxdb(loop, wavq, influxhost, influxport, wavEncode))
+
+    loop.run_forever()
+
+async def recvFromZmq(loop, queue, host, port):
     ctx = zmq.asyncio.Context()
-    sock = ctx.socket(zmq.SUB)
+    sock = ctx.socket(zmq.SUB, io_loop=loop)
     #sock.connect(f'tcp://{host}:{port}')
     sock.bind(f'tcp://{host}:{port}')
     sock.subscribe(b'')
-    while True:
+    while loop.is_running():
         msg = await sock.recv()
         await queue.put(msg)
+
+async def sendToInfluxdb(loop, queue, host, port, encoder):
+    udpproto = lambda: asyncio.DatagramProtocol()
+    transport, proto = await loop.create_datagram_endpoint(udpproto, remote_addr=(host, port))
+    while loop.is_running():
+        msg = await queue.get()
+        print(f'Received {msg}')
+        try:
+            decoded = decodeMsg(msg)
+            line = encoder(decoded)
+        except ValueError:
+            print(f'Failed to parse: {msg}')
+            continue
+        print(f'Sending: {line}')
+        transport.sendto(line)
+    transport.close()
 
 def decodeMsg(msg, wave=False):
     # Parse incoming message
@@ -30,26 +62,27 @@ def decodeMsg(msg, wave=False):
     frame['topic'] = topic
     return frame
 
-def numericToInfluxLine(frame):
+def numEncode(frame):
+    # Encode numerics for InfluxDB
     metadata = frame['meta'].copy()
     metadata['origin'] = frame['topic']
     tags = [f"{tag}={value}" for tag, value in metadata.items()]
+    str_tags = ','.join(tags)
     data = frame['data']
     fields = [f"{field}={value}" for field, value in data.items()]
-    str_tags = ','.join(tags)
     str_fields = ','.join(fields)
     time = frame['basetime']
     line = f'numerics,{str_tags} {str_fields} {time}'
     return line.encode('ASCII')
 
-def waveToInfluxLines(frame):
+def wavEncode(frame):
+    # Encode waves for InfluxDB
     metadata = frame['meta'].copy()
     metadata['origin'] = frame['topic']
     tags = [f"{tag}={value}" for tag, value in metadata.items()]
     str_tags = ','.join(tags)
     #basetime = frame['basetime']
-    wavedata = pd.DataFrame(frame['data'])
-    wavedata = wavedata.set_index('time')
+    wavedata = pd.DataFrame(frame['data']).set_index('time')
     lines = []
     for time, waves in wavedata.iterrows():
         fields = []
@@ -61,28 +94,6 @@ def waveToInfluxLines(frame):
     lineagg = '\n'.join(lines)
     return lineagg.encode('ASCII')
 
-async def sendToInfluxdb(loop, queue, host, port):
-    udpproto = lambda: asyncio.DatagramProtocol()
-    transport, proto = await loop.create_datagram_endpoint(udpproto, remote_addr=(host, port))
-    while True:
-        msg = await queue.get()
-        print(f'Received {msg}')
-        decoded = decodeMsg(msg)
-        line = waveToInfluxLines(decoded)
-        print(f'Sending: {line}')
-        transport.sendto(line)
-    transport.close()
 
-
-loop = asyncio.get_event_loop()
-queue = asyncio.Queue(loop=loop)
-zmq_coro = recvFromZmq(queue, host, zmqport)
-influx_coro = sendToInfluxdb(loop, queue, host, influxport)
-g = asyncio.gather(zmq_coro, influx_coro)
-
-try:
-    loop.run_until_complete(g)
-except KeyboardInterrupt:
-    print('Ctrl-C pressed')
-finally:
-    loop.close()
+if __name__ == "__main__":
+    run()
